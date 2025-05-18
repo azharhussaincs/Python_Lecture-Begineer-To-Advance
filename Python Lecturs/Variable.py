89d833ae90204df036cad26e5a8ccd3f1adcfd30
Azhar <azharhussaincs@gmail.com>
2025-05-19 03:33:49 +0500

    Pushing all codes

--- Python Lecturs/Variable.py
+++ Python Lecturs/Variable.py
@@ -1,7 +1,17 @@
-Name ="Azhar Hussain"
+Name ="My Students"
 Age=26
 print(Name)
 a=20
 b=50
 c=a+b
 print(c)
+Email='azharhussaincs@gmail.com'
+Password="MyPassword.11899"
+# in Java how we create variable 
+# String Email="azharhussaincs@gmail.com"
+student_Name="Ali Hmaza"
+print(student_Name)
+d=50
+print(d)
+m=d*c
+print(m)
