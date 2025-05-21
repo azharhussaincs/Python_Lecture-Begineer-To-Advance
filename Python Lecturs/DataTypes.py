2b4ec6d5ea2e8b5f6b1ae65b227aad6a161558cc
Azhar <azharhussaincs@gmail.com>
2025-05-21 18:12:40 +0500

    New Lecture Is ubloaded On data types in python

---
+++ Python Lecturs/DataTypes.py
@@ -0,0 +1,44 @@
+Student_Age=26
+print(Student_Age)
+#Now I am Checking types of data
+print(type(Student_Age))
+#Now I am creating floting number 
+Student_Marks=88.789
+print(Student_Marks)
+print(type(Student_Marks))
+#complex Number 
+z = 2 + 3j
+print(z)
+print(type(z))
+#text data Types
+Student_Name="My Students "
+print(Student_Name)
+print(type(Student_Name))
+#Second method
+Teacher_Name='Azhar Hussain'
+print(Teacher_Name)
+print(type(Teacher_Name))
+# Bol Data type
+is_active = True
+is_admin = False
+print(is_active)
+print(type(is_active))
+print(type(is_admin))
+#Sequence Types
+Student_List=["Nimra", "Fatima", "Sana", "Adnan"]
+print(Student_List)
+print(type(Student_List))
+Student_Tuple=("Nimra", "Fatima", "Sana", "Adnan")
+print(Student_Tuple)
+print(type(Student_Tuple))
+#Set Type
+S_set={1,2,3,4,8,5,5,8}
+print(S_set)
+print(type(S_set))
+#Mapping Type
+Student_Details={"S-Name":"AneelaZubair",
+                 "Age":15,
+                 "Class":10,
+                 "City":"Islamabad"}
+print(Student_Details)
+print(type(Student_Details))
